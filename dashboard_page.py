758361137b8095ad5783ebe3a5202d2152ce659e
AutomatedTester <dburns@mozilla.com>
2011-08-09 00:21:45 +0100

    removed unused methods

--- dashboard_page.py
+++ dashboard_page.py
@@ -6,7 +6,6 @@ from selenium.common.exceptions import NoSuchElementException
 
 class DashboardPage(Page):
 
-    _account_name = "//div/aside[@id='app-sidebar']/h2"
     _home_page_click = 'flightdeck-logo'
     _top_public_addon_name = "//section[@id='app-content']/ul/li[1]/h3"
     _top_public_lib_name = "//section[@id='app-content']/ul[2]/li[1]/h3"
@@ -37,50 +36,27 @@ class DashboardPage(Page):
     def get_page_title(self):
         return self.sel.title
     
-    def get_account_name(self):
-        return self.sel.find_element_by_xpath(self._account_name)
-    
-    def navigate_to_homepage(self):
-        self.sel.find_element_by_id(self._home_page_click).click()
-        self.sel.implicitly_wait(10)
-    
     def get_top_addon_name(self):
-        #self.sel.find_element_by_xpath(self._create_addon_btn).click()
         return self.sel.find_element_by_xpath(self._top_public_addon_name).text
     
     def get_top_lib_name(self):
-        #self.sel.find_element_by_xpath(self._create_addon_btn).click()
         return self.sel.find_element_by_xpath(self._lib_label_name).text
     
     def get_addon_label_name(self):
-        #self.sel.find_element_by_xpath(self._create_addon_btn).click()
         return self.sel.find_element_by_xpath(self._lib_label_name)
     
     def get_lib_label_name(self):
-        #self.sel.find_element_by_xpath(self._create_addon_btn).click()
         return self.sel.find_element_by_xpath(self._lib_label_name)
     
     def get_addons_count(self):
-        #self.sel.find_element_by_xpath(self._create_addon_btn).click()
         return self.sel.find_element_by_xpath(self._addons_counter).text
         
-    def get_libs_count(self):
-        #self.sel.find_element_by_xpath(self._create_addon_btn).click()
-        return self.sel.find_element_by_xpath(self._libs_counter).text
-        
     def calc_total_addons(self):
-        #self.sel.find_element_by_xpath(self._create_addon_btn).click()
         elements = self.sel.find_elements_by_xpath(self._addons_list)
         return len(elements)
         
-    def calc_total_libs(self):
-        #self.sel.find_element_by_xpath(self._create_addon_btn).click()
-        elements = self.sel.find_elements_by_xpath(self._libs_list)
-        return len(elements)
-    
     def go_to_private_addons_page(self):
         self.sel.find_element(*self._private_addons_link).click()
-        self.sel.implicitly_wait(10)
     
     def go_to_private_libs_page(self):
         self.sel.find_element(*self._private_libs_link).click()
@@ -90,11 +66,9 @@ class DashboardPage(Page):
     
     def navigate_to_addon_editor(self):
         self.sel.find_element(*self._lib_edit_btn_locator).click()
-        self.sel.implicitly_wait(10)
     
     def navigate_to_lib_editor(self):
         self.sel.find_element(*self._lib_edit_btn_locator).click()
-        self.sel.implicitly_wait(10)
         
     def check_addon_test_btn_present(self):
         try:
@@ -131,56 +105,17 @@ class DashboardPage(Page):
         except NoSuchElementException:
             return False
     
-    def check_lib_edit_btn_present(self):
-        try:
-            self.sel.find_element_by_xpath(self._lib_edit_btn)
-            return True
-        except NoSuchElementException:
-            return False
-    
-    def check_lib_delete_btn_present(self):
-        try:
-            self.sel.find_element_by_xpath(self._lib_delete_btn)
-            return True
-        except NoSuchElementException:
-            return False
-    
-    def check_lib_public_btn_present(self):
-        try:
-            self.sel.find_element_by_xpath(self._lib_public_btn)
-            return True
-        except NoSuchElementException:
-            return False
-    
-    def check_lib_private_btn_present(self):
-        try:
-            self.sel.find_element_by_xpath(self._lib_private_btn)
-            return True
-        except NoSuchElementException:
-            return False
-    
     def click_addon_mkprivate_btn(self):
         self.sel.find_element(*self._lib_private_btn_locator).click()
-        self.sel.implicitly_wait(10)
-        return
     
     def click_lib_mkprivate_btn(self):
         self.sel.find_element(*self._lib_private_btn_locator).click()
-        self.sel.implicitly_wait(10)
-        return
 
     def click_addon_delete(self):
         self.sel.find_element(*self._addon_delete_btn).click()
-        self.sel.implicitly_wait(10)
-        return
 
     def confirm_delete(self):
         self.sel.find_element_by_id(self._confirm_delete_btn).click()
-        self.sel.implicitly_wait(10)
-        return
 
     def click_lib_delete(self):
         self.sel.find_element(*self._addon_delete_btn).click()
-        self.sel.implicitly_wait(10)
-        return
-    
