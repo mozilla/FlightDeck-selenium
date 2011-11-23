ec45d6da5d7e56097fb521bbf23626d3466ad3db
Zac <zac@zac-Ubuntu.(none)>
2011-11-23 11:56:10 +0000

    Change licence names

--- pages/regions/editor_tab_region.py
+++ pages/regions/editor_tab_region.py
@@ -18,8 +18,7 @@
 # Portions created by the Initial Developer are Copyright (C) 2011
 # the Initial Developer. All Rights Reserved.
 #
-# Contributor(s): David Burns
-#                 Zac Campbell
+# Contributor(s): Zac Campbell
 #
 # Alternatively, the contents of this file may be used under the terms of
 # either the GNU General Public License Version 2 or later (the "GPL"), or
