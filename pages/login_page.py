1efbe7d4cf7af5c32a7073f0e1f603b2f1fcb811
Zac <zac@zac-Ubuntu.(none)>
2012-04-23 12:04:47 +0100

    Adjust to use plugin timeout Fix typos in messages.

--- pages/login_page.py
+++ pages/login_page.py
@@ -28,8 +28,8 @@ class LoginPage(FlightDeckBasePage):
         pop_up = BrowserID(self.testsetup.selenium, self.testsetup.timeout)
         pop_up.sign_in(credentials['email'], credentials['password'])
 
-        WebDriverWait(self.selenium, 10).until(lambda s: self.header.logged_in,
-            'BrowserID did not login before the timeout')
+        WebDriverWait(self.selenium, self.timeout).until(lambda s: self.header.logged_in,
+            'Timed-out waiting for the login to complete')
 
         from pages.dashboard_page import DashboardPage
         return DashboardPage(self.testsetup)
